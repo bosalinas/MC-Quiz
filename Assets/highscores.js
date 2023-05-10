826f6cc523fb9a99acc47567a6460101cb34d50f
Freebird <bo.salinas12@gmail.com>
2023-05-10 18:15:58 -0500

    HS java and HTML files, CSS stylings

---
+++ Assets/highscores.js
@@ -0,0 +1,11 @@
+function highScores() {
+    const initials = JSON.parse(localStorage.getItem("intitials"));
+    const score = JSON.parse(localStorage.getItem("score"));
+
+}
+//btn to clear scores
+function clearHighScores() {
+
+}
+
+//go back btn eventlistener
